08a3766cd0fb78a6e527841300aebe0d8fb5b11a
wendel5001 <wendelywulur@gmail.com>
2023-10-04 20:09:39 +0800

    first commit

--- lib/view/profile_page.dart
+++ lib/view/profile_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-
 import '../theme.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,47 +13,117 @@ class ProfilePage extends StatelessWidget {
           height: double.infinity,
           decoration: const BoxDecoration(
             image: DecorationImage(
-                image: AssetImage(
-                  'assets/background.png',
-                ),
-                fit: BoxFit.cover),
+              image: AssetImage('assets/bgasset.jpeg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Positioned(
+          top: 20,
+          right: 20,
+          child: Image.asset(
+            'logounity.png',
+            width: 40,
+            height: 40,
           ),
         ),
         Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const CircleAvatar(
-                radius: 55,
-                backgroundImage: AssetImage('assets/lens.jpeg'),
+              Container(
+                width: 110,
+                height: 110,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 2,
+                  ),
+                  image: DecorationImage(
+                    image: AssetImage('assets/Nahida.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-              const SizedBox(
-                height: 5,
-              ),
+              const SizedBox(height: 5),
               Text(
-                'Lens',
+                'Wendell',
                 style: titleTextStyle.copyWith(
-                    letterSpacing: 2,
-                    color: Colors.deepPurple,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w800),
+                  letterSpacing: 2,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              const SizedBox(
-                height: 10,
-              ),
+              const SizedBox(height: 10),
               Text(
-                'Senior Flutter Developer',
+                '221011060132',
                 style: titleTextStyle.copyWith(
-                    color: Colors.black, fontWeight: FontWeight.bold),
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              const SizedBox(
-                height: 5,
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SocialMediaIcon(imagePath: 'instagram.png', text: '@wends_w'),
+                  SocialMediaIcon(imagePath: 'twitter.png', text: '@wendss_w'),
+                  SocialMediaIcon(imagePath: 'facebook.png', text: 'wendss'),
+                ],
               ),
-              const Text('nyenyenyenyenye sipaling flutter')
             ],
           ),
-        )
+        ),
       ],
+    );
+  }
+}
+
+class SocialMediaIcon extends StatelessWidget {
+  final String imagePath;
+  final String text;
+
+  const SocialMediaIcon({
+    required this.imagePath,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          Container(
+            width: 50,
+            height: 50, 
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(
+                  8),
+              color: Colors.white, 
+            ),
+            child: Center(
+              child: Image.asset(
+                imagePath,
+                width: 24, 
+                height: 24,
+              ),
+            ),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter_windows_3.7.11-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\mobdev\\mobiledev_flutter_1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter_windows_3.7.11-stable\\flutter"
+  "PROJECT_DIR=C:\\mobdev\\mobiledev_flutter_1"
+  "FLUTTER_ROOT=C:\\flutter_windows_3.7.11-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\mobdev\\mobiledev_flutter_1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\mobdev\\mobiledev_flutter_1"
+  "FLUTTER_TARGET=C:\\mobdev\\mobiledev_flutter_1\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\mobdev\\mobiledev_flutter_1\\.dart_tool\\package_config.json"
+)
